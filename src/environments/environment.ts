73d15c453011c73aefc95a1b83b867957cafbff6
Frescobarh <fescobar@applaudostudios.com>
2023-05-16 16:08:07 -0500

    Firebase Conf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'expense-tracker-f9535',
+    appId: '1:1065775068923:web:b486d8dc19e6a87edb225e',
+    storageBucket: 'expense-tracker-f9535.appspot.com',
+    apiKey: 'AIzaSyCmd3lu8VvBIX9YpYPkCYJC0A2MTYRR55Y',
+    authDomain: 'expense-tracker-f9535.firebaseapp.com',
+    messagingSenderId: '1065775068923',
+  },
   production: false
 };
 
