3c069f1cf66c87bc27c5837e28cc077865da3953
Frescobarh <fescobar@applaudostudios.com>
2023-05-16 16:07:51 -0500

    Added Firebase Configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'expense-tracker-f9535',
+    appId: '1:1065775068923:web:b486d8dc19e6a87edb225e',
+    storageBucket: 'expense-tracker-f9535.appspot.com',
+    apiKey: 'AIzaSyCmd3lu8VvBIX9YpYPkCYJC0A2MTYRR55Y',
+    authDomain: 'expense-tracker-f9535.firebaseapp.com',
+    messagingSenderId: '1065775068923',
+  },
   production: true
 };
