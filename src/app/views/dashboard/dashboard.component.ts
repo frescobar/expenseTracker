fc2405af5e05862d6415777778017a63f68a5420
Frescobarh <fescobar@applaudostudios.com>
2023-05-23 17:49:03 -0500

    created dashboard view and module

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { User, onAuthStateChanged } from '@angular/fire/auth';
+import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private afAuth: Auth,
+    private sharedService: SharedService,
+    private router: Router
+  ) {}
+  currentUser: User | null = null;
+  isToggle = false;
+  unSubscribe$ = new Subject();
 
   ngOnInit(): void {
+    onAuthStateChanged(this.afAuth, (user) => {
+      this.currentUser = user;
+      localStorage.setItem('userId', this.currentUser?.uid || '');
+    });
+    this.sharedService.isToggle$
+      .pipe(takeUntil(this.unSubscribe$))
+      .subscribe((isToggle) => {
+        this.isToggle = isToggle;
+      });
   }
-
+  logout(): void {
+    this.afAuth.signOut();
+    this.router.navigate(['/login']);
+  }
+  setIsToggle(): void {
+    const isToggle = !this.isToggle;
+    this.sharedService.setIsToggle(isToggle);
+  }
+  ngOnDestroy(): void {
+    this.unSubscribe$.next(true);
+    this.unSubscribe$.complete();
+  }
 }
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,9 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutes } from './dashboard.routing.module';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { ExpenseComponent } from './expense/expense.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NgChartsModule } from 'ng2-charts';
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutes],
+  declarations: [DashboardComponent, ExpenseComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutes,
+    NzLayoutModule,
+    NzMenuModule,
+    ComponentsModule,
+    NzIconModule,
+    NzButtonModule,
+    NzTableModule,
+    NzDividerModule,
+    NgChartsModule,
+  ],
+  exports: [DashboardComponent, ExpenseComponent],
 })
 export class DashboardModule {}
