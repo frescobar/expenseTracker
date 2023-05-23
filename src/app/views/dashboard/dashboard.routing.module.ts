7e837f6dda632dead9b742a84bf42a3e1e47dc8a
Frescobarh <fescobar@applaudostudios.com>
2023-05-23 17:50:43 -0500

    created children route for dashboard

--- src/app/views/dashboard/dashboard.routing.module.ts
+++ src/app/views/dashboard/dashboard.routing.module.ts
@@ -1,10 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'expenses',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     component: DashboardComponent,
+    children: [
+      {
+        path: 'expenses',
+        component: ExpenseComponent,
+      },
+    ],
   },
 ];
 
