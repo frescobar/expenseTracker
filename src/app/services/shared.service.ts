eb9c4af6991057dfa50e562d2537a555ea7b9f1f
Frescobarh <fescobar@applaudostudios.com>
2023-05-23 17:50:10 -0500

    Created Behaviour subjects for propeties shared across the application

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  private isToggleSubject = new BehaviorSubject<boolean>(false);
+  isToggle$ = this.isToggleSubject.asObservable();
+
+  private isVisibleSubject = new BehaviorSubject<boolean>(false);
+  isVisible$ = this.isVisibleSubject.asObservable();
+
+  private showChartsSubject = new BehaviorSubject<boolean>(false);
+  showCharts$ = this.showChartsSubject.asObservable();
+
+  setIsToggle(value: boolean) {
+    this.isToggleSubject.next(value);
+  }
+
+  setIsVisible(value: boolean) {
+    this.isVisibleSubject.next(value);
+  }
+
+  setShowCharts(value: boolean) {
+    this.showChartsSubject.next(value);
+  }
+}
